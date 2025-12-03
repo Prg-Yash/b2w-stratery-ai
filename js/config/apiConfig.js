45f2dd7a49bb12347ba28f942988f7048f60db33
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2025-12-04 03:48:34 +0530

    DONE

---
+++ js/config/apiConfig.js
@@ -0,0 +1,6 @@
+export const API_CONFIG = {
+  // I would normally not commit API keys to source control.
+  // This is just for demonstration purposes.
+  // I would have setup cloudfare for this, but was too bored to do it. Either way i am going to delete it later.
+  GEMINI_API_KEY: "AIzaSyCU8atkEv2nB5o_KEowsDVX_RwvPgfTNl0",
+};
