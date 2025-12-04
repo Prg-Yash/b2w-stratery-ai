38aaa2aaf7910eed15b30225e143cd29f7a4b0e9
Yash Nimse <127442680+Prg-Yash@users.noreply.github.com>
2025-12-04 02:52:45 -0500

    Changes

--- js/config/apiConfig.js
+++ js/config/apiConfig.js
@@ -2,5 +2,6 @@ export const API_CONFIG = {
   // I would normally not commit API keys to source control.
   // This is just for demonstration purposes.
   // I would have setup cloudfare for this, but was too bored to do it. Either way i am going to delete it later.
-  GEMINI_API_KEY: "AIzaSyCU8atkEv2nB5o_KEowsDVX_RwvPgfTNl0",
+  GEMINI_API_KEY: "AIzaSyD_Br6qf4QSYpK8Z1uYY50kYfwKA9q_Hqs",
 };
+
